168ae158d1e9b6e612c0c7c346d0b3261878cbd8
amirkey_ <galaxyman0110@gmail.com>
2022-06-16 17:44:19 +0430

    image_uploader

--- imageuploader/settings.py
+++ imageuploader/settings.py
@@ -206,4 +206,4 @@ EMAIL_HOST_PASSWORD = 'airbawmsrlmwebxh'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
